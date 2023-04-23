e60512c91ceb5d17ed86746131205d743f4c3f34
Sohan <sohan.anampalli@gmail.com>
2023-04-23 17:54:47 +0200

    commit

---
+++ Javascript/popup.js
@@ -0,0 +1,26 @@
+function OpenPopup(name) {
+    document.getElementById('popup-blur').style.display = 'block';
+    document.querySelector(".popup."+name).style.display = 'block';
+}
+
+function ClosePopups() {
+    for (var i = 0; i < document.getElementById('popup-blur').childElementCount; i++) {
+        document.getElementById('popup-blur').children[i].style.display = 'none';
+    }
+    document.getElementById('popup-blur').style.display = 'none';
+}
+var openedTicket = null;
+function ShowTicket(title, content, answer, id) {
+    document.getElementById("ticket-title").innerHTML = title;
+    document.getElementById("ticket-content").innerHTML = content;
+    document.getElementById("ticket-answer").innerHTML = answer;
+    document.getElementById("ticket-answer").style.color = "white";
+    document.getElementById("ticket-close-button").innerHTML = "Fermer cette question";
+    if (answer === "") {
+        document.getElementById("ticket-close-button").innerHTML = "Supprimer cette question";
+        document.getElementById("ticket-answer").innerHTML = "Notre equipe n'a pas encore eu le temps de répondre.";
+        document.getElementById("ticket-answer").style.color = "grey";
+    }
+    OpenPopup("ticket");
+    openedTicket = id;
+}
