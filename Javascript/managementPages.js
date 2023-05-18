8aa84cca04f048de96ec36fec48c89382ccb6d31
ArseneBrosy <arsene.brosy@divtec.ch>
2023-05-18 18:34:09 +0200

    esc

--- Javascript/managementPages.js
+++ Javascript/managementPages.js
@@ -1,7 +1,6 @@
 function ShowUser(jsonValues) {
     const user = JSON.parse(jsonValues);
-    console.log(user);
-
+    
     document.getElementById("user-page-title").innerHTML = "<button onclick='OpenDashboard(0)'>&#129044;</button>" + user.prenom + " " + user.nom;
     for (let i = 0; i < Object.keys(user).length; i++) {
         document.getElementById("user-page-" + Object.keys(user)[i]).innerHTML = Object.values(user)[i];
