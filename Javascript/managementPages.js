f908f402be73945c0240819582f44b4b41656029
ArseneBrosy <arsene.brosy@divtec.ch>
2023-05-21 20:39:25 +0200

    button

--- Javascript/managementPages.js
+++ Javascript/managementPages.js
@@ -1,12 +1,14 @@
 function ShowUser(jsonValues) {
     const user = JSON.parse(jsonValues);
 
-    document.getElementById("user-pageTitle").innerHTML = "<button onclick='OpenDashboard(0)'>&#129044;</button>" + user.prenom + " " + user.nom;
+    document.getElementById("user-pageTitle").innerHTML = "<button class='backArrow' onclick='OpenDashboard(0)'>&#129044;</button>" + user.prenom + " " + user.nom;
     for (let i = 0; i < Object.keys(user).length; i++) {
         if (document.getElementById("user-page-" + Object.keys(user)[i]) != null) {
             document.getElementById("user-page-" + Object.keys(user)[i]).innerHTML = Object.values(user)[i];
         }
     }
+
+    document.getElementById("user-page-status").innerHTML += "<button style='margin-left: 50px'>Passer à la suite</button>"
 
     OpenDashboard(3);
 }
@@ -16,7 +18,7 @@ function ShowTicket(jsonValues) {
     const ticket = JSON.parse(jsonValues);
     openedTicketId = ticket.id;
     
-    document.getElementById("ticket-pageTitle").innerHTML = "<button onclick='OpenDashboard(1)'>&#129044;</button>" + ticket.title;
+    document.getElementById("ticket-pageTitle").innerHTML = "<button class='backArrow' onclick='OpenDashboard(1)'>&#129044;</button>" + ticket.title;
     for (let i = 0; i < Object.keys(ticket).length; i++) {
         if (document.getElementById("ticket-page-" + Object.keys(ticket)[i]) != null) {
             document.getElementById("ticket-page-" + Object.keys(ticket)[i]).innerHTML = Object.values(ticket)[i];
